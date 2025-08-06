d88e907b773b576134c823bce3975bc5aa05b055
Siar <siar019283@gmail.com>
2025-08-06 21:35:14 +0900

    bump notes to 1.1.1

--- Casks/amphi-notes.rb
+++ Casks/amphi-notes.rb
@@ -1,6 +1,6 @@
 cask "amphi-notes" do
-  version "1.1.0"
-  sha256 "b7caa3d498cfce4278219415487f0b90807652182efd280f17febf79d2ece5b4"
+  version "1.1.1"
+  sha256 "b76d52641e0b478c914a8adff8cd1e08c1168e755d4124d4df26e4e4724f4b06"
 
   url "https://amphi.site/releases/notes/notes-macos-#{version}.dmg"
   name "Amphi Notes"
